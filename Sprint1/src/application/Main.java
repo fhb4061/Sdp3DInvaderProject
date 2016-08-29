29538af9d5f9b6118b3268fcadfaf537595259cb
fhb4061 <fhb4061@172.30.112.180>
2016-08-29 14:22:09 +1200

    testing branch 2

--- Sprint1/src/application/Main.java
+++ Sprint1/src/application/Main.java
@@ -105,7 +105,7 @@ public class Main extends Application{
 	int currHitsOnTank =0;                                               //the amount of hits on the tank since last update
 
 	
-	////testing Jonathan lopeti
+	////testing Jonathan lopeti 3
 	
 	
 	public static void main(String[] args)//DO NOT CODE HERE
